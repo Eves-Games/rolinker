e17256a11070789c5d458fe55de39a0c1f2ad071
zekepari <pariezekiel@gmail.com>
2024-02-28 01:50:27 +1000

    change guilds page into grid, get rid of icons, make whole thing clickable and upon click call specific guild api to check if bot is inside guild

--- src/app/manage/guilds/[id]/loading.tsx
+++ src/app/manage/guilds/[id]/loading.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function Loading() {
-  return <div>Loading Server</div>
+  return <div>Loading Guild</div>
 }
--- src/app/manage/guilds/page.tsx
+++ src/app/manage/guilds/page.tsx
@@ -1,11 +1,23 @@
-import prisma from "@/lib/db";
-import { PlusIcon, PencilIcon } from '@heroicons/react/24/outline';
 import { APIGuild } from "discord-api-types/v10"
-import Link from 'next/link';
 import Image from 'next/image';
 import { auth } from "@/auth";
+import { redirect } from 'next/navigation';
 
 export const runtime = "edge";
+
+async function checkGuild(guild: APIGuild) {
+    const guildRes = await fetch(`https://discord.com/api/v10/guilds/${guild.id}/preview`, {
+        headers: {
+            Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
+        }
+    });
+
+    if (!guildRes.ok) {
+        redirect(`https://discord.com/api/oauth2/authorize?scope=bot+applications.commands&client_id=990855457885278208&permissions=8&guild_id=${guild.id}&disable_guild_select=true&redirect_uri=https://rolinker.net/api/auth/guild&response_type=code`)
+    };
+
+    redirect(`/manage/guilds/${guild.id}`)
+}
 
 export default async function Page() {
     const session = await auth();
@@ -23,74 +35,26 @@ export default async function Page() {
     const guilds: Array<APIGuild> = await guildsRes.json();
     const ownedGuilds: Array<APIGuild> = guilds.filter(guild => guild.owner === true);
 
-    const fetchGuildPreview = async (guild: APIGuild) => {
-        const guildRes = await fetch(`https://discord.com/api/v10/guilds/${guild.id}/preview`, {
-            headers: {
-                Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
-            }
-        });
-    
-        return await guildRes.json();
-    };
-    
-    const activeGuildsPromises = ownedGuilds.map(guild => fetchGuildPreview(guild));
-    const activeGuilds = await Promise.all(activeGuildsPromises);
-    
-    console.log(activeGuilds);
-
-    if (guilds.length === 0) {
-        return (
-            <a href='https://discord.com/api/oauth2/authorize?client_id=990855457885278208&permissions=8&scope=bot+applications.commands' target='_blank' className="px-4 py-2 w-full flex justify-center items-center transition hover:bg-neutral-700 bg-neutral-800 rounded shadow-lg">
-                <PlusIcon className="h-16 w-6" />
-            </a>
-        )
-    }
-
-    const guildIds: Array<string> = guilds.map(guild => guild.id);
-
-
-    const knownGuilds = await prisma.guild.findMany({
-        where: {
-            id: {
-                in: guildIds
-            }
-        }
-    });
-
-    knownGuilds.sort((guildA, guildB) => {
-        if (guildA.ownerId === session?.user.id) {
-            return -1;
-        } else if (guildB.ownerId === session?.user.id) {
-            return 1;
-        } else {
-            return 0;
-        }
-    });
-
     return (
-        <div className='flex-col space-y-2 w-full'>
-            {knownGuilds.map((guild) => (
-                <div key={guild.id} className='flex items-center justify-between space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg'>
-                    <div className='flex items-center space-x-4'>
-                        {guild.imageUrl ? (
-                            <Image src={guild.imageUrl} alt={`${guild.name} Icon`} className='h-16 w-16 rounded' width={100} height={100} />
+        <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-2 w-full'>
+            {ownedGuilds.length !== 0 && ownedGuilds.map((guild) => (
+                <form key={guild.id} className={`${ownedGuilds.length === 1 && 'col-span-full'}`} action={async () => {
+                    'use server';
+
+                    await checkGuild(guild);
+                }}>
+                    <button className={`flex items-center space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg hover:bg-neutral-700`}>
+                        {guild.icon ? (
+                            <Image src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt={`${guild.name} Icon`} className='h-16 w-16 rounded' width={100} height={100} />
                         ) : (
                             <div className='h-16 w-16 flex items-center justify-center'>
                                 <span className='text-4xl'>{guild.name.charAt(0)}</span>
                             </div>
                         )}
                         <span className='text-lg'>{guild.name}</span>
-                    </div>
-                    {guild.ownerId === session?.user.id && (
-                        <Link href={`/manage/guilds/${guild.id}`} className="flex items-center space-x-2 px-2 py-2 transition rounded hover:bg-neutral-700">
-                            <PencilIcon className="h-6" />
-                        </Link>
-                    )}
-                </div>
+                    </button>
+                </form>
             ))}
-            <a href='https://discord.com/api/oauth2/authorize?client_id=990855457885278208&permissions=8&scope=bot+applications.commands' target='_blank' className="px-4 py-2 w-full flex justify-center items-center transition hover:bg-neutral-700 bg-neutral-800 rounded shadow-lg">
-                <PlusIcon className="h-16 w-6" />
-            </a>
         </div>
     );
 };
