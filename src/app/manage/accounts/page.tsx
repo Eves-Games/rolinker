cef4ce194ebebdd8005d1fe6453cc4ca2283c5b2
zekepari <pariezekiel@gmail.com>
2024-02-28 10:48:41 +1000

    simplify for logic refactor

--- src/app/manage/accounts/page.tsx
+++ src/app/manage/accounts/page.tsx
@@ -151,40 +151,17 @@ export default async function Page() {
   return (
     <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-2 w-full'>
       {doneAccounts.map((account) => (
-        <div key={account.id} className='flex items-center justify-between space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg'>
+        <button key={account.id} className='flex items-center justify-between space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg hover:bg-neutral-700'>
           <div className='flex items-center space-x-4'>
             <Image src={account.imageUrl} alt='Avatar Icon' className='h-16 w-16 rounded' width={100} height={100} />
             <span className='text-lg'>{account.name}</span>
           </div>
-          <div className='flex items-center space-x-2'>
-            {account.isPrimary ? (
-              <div className="px-2 py-2 transition rounded">
-                <SolidStarIcon className="h-6" />
-              </div>
-            ) : (
-              <form action={async () => {
-                'use server';
-
-                await setPrimary(account.id);
-                await revalidatePath('/');
-              }}>
-                <button className="px-2 py-2 transition hover:bg-neutral-700 rounded">
-                  <StarIcon className="h-6" />
-                </button>
-              </form>
-            )}
-            <form action={async () => {
-              'use server';
-
-              await deleteAccount(account.id);
-              await revalidatePath('/');
-            }}>
-              <button className="px-2 py-2 transition hover:bg-neutral-700 rounded">
-                <TrashIcon className="h-6 stroke-red-500" />
-              </button>
-            </form>
-          </div>
-        </div>
+          {account.isPrimary && (
+            <div className="px-2 py-2 transition rounded">
+              <SolidStarIcon className="h-6" />
+            </div>
+          )}
+        </button>
       ))}
       <form className='col-span-full' action={async () => {
         'use server';
