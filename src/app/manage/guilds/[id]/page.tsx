75b125d4b4c49147ed2fe4e4040fe3a1e7a8c36e
zekepari <pariezekiel@gmail.com>
2024-02-29 22:11:31 +1000

    simplification

--- src/app/manage/guilds/[id]/page.tsx
+++ src/app/manage/guilds/[id]/page.tsx
@@ -1,134 +1,23 @@
-import prisma from "@/lib/db";
-import { UserGroup } from "@/roblox-api";
-import Image from 'next/image';
-import { HashtagIcon, ArrowPathIcon, UserGroupIcon, ServerIcon } from '@heroicons/react/24/outline';
-import { StarIcon as SolidStarIcon } from '@heroicons/react/24/solid';
 import { auth } from "@/auth";
+import { getBotGuild } from "@/lib/guilds";
+import { APIGuild } from "discord-api-types/v10";
 
 export const runtime = "edge";
 
 export default async function Page({ params }: { params: { id: string } }) {
     const session = await auth();
 
-    const guild = await prisma.guild.findUnique({
-        where: {
-            id: params.id
-        }
-    })
+    const guild: APIGuild = await getBotGuild(params.id);
 
-    if (!guild || guild.ownerId != session?.user.id) {
-        return <div>No Permission</div>
-    }
-
-    const accounts = await prisma.account.findMany({
-        where: {
-            ownerId: session?.user.id
-        },
-        orderBy: {
-            isPrimary: 'desc'
-        }
-    });
-
-    const userGroupsPromises = accounts.map(async account => {
-        return await fetch(`https://groups.roblox.com/v2/users/${account.id}/groups/roles?includeLocked=true`)
-            .then(response => { return response.json(); });
-    });
-
-    const userGroupsResponses = await Promise.all(userGroupsPromises);
-
-    const userGroups: Array<UserGroup> = userGroupsResponses
-        .map(response => response.data)
-        .reduce((acc, current) => [...acc, ...current], [])
-        .filter((userGroup: UserGroup) => userGroup.role.rank == 255);
+    if (guild.owner_id !== session?.user.id) {
+        return (
+            <div>No permission</div>
+        )
+    };
 
     return (
         <div className='flex-col space-y-2 w-full'>
-            <div>{session.user.id}</div>
-            <div className='flex items-center justify-between space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg'>
-                <div className='flex items-center space-x-4'>
-                    {guild.imageUrl ? (
-                        <Image src={guild.imageUrl} alt={`${guild.name} Icon`} className='h-16 w-16 rounded' width={100} height={100} />
-                    ) : (
-                        <div className='h-16 w-16 flex items-center justify-center'>
-                            <span className='text-4xl'>{guild.name.charAt(0)}</span>
-                        </div>
-                    )}
-                    <span className='text-lg'>{guild.name}</span>
-                </div>
-                <button className='flex items-center space-x-2 px-2 py-2 rounded hover:bg-neutral-600'>
-                    <ArrowPathIcon className='h-6' />
-                    <span>Generate Roles</span>
-                </button>
-            </div>
-
-            <div className='flex space-x-4 items-start justify-center'>
-                <div className='space-y-2 grow'>
-                    <div className='px-4 py-2 bg-neutral-800 rounded shadow-lg text-lg text-center'>
-                        Link Roblox Group
-                    </div>
-                    <div className='bg-neutral-800 flex items-center space-x-4 w-full px-4 py-2 rounded shadow-lg'>
-                        <SolidStarIcon className='h-6' />
-                        <span>Army</span>
-                    </div>
-                    <div className='bg-neutral-800 rounded shadow-lg overflow-y-scroll h-96 scrollbar-none'>
-                        {userGroups.map((userGroup, index) => (
-                            <button
-                                key={userGroup.group.id}
-                                className={`flex items-center space-x-4 w-full px-4 py-2 hover:bg-neutral-600 ${index === 0 ? 'rounded-t' : ''
-                                    } ${index === userGroups.length - 1 ? 'rounded-b' : ''
-                                    }`}
-                            >
-                                <UserGroupIcon className='h-6' />
-                                <span>{userGroup.group.name}</span>
-                            </button>
-                        ))}
-                    </div>
-                </div>
-                <div className='space-y-2 grow'>
-                    <div className='px-4 py-2 bg-neutral-800 rounded shadow-lg text-lg text-center'>
-                        Set Invite Channel
-                    </div>
-                    <div className='bg-neutral-800 flex items-center space-x-4 w-full px-4 py-2 rounded shadow-lg'>
-                        <SolidStarIcon className='h-6' />
-                        <span>┃📢army-wide</span>
-                    </div>
-                    <div className='bg-neutral-800 rounded shadow-lg overflow-y-scroll h-96 scrollbar-none'>
-                        {userGroups.map((userGroup, index) => (
-                            <button
-                                key={userGroup.group.id}
-                                className={`flex items-center space-x-4 w-full px-4 py-2 hover:bg-neutral-600 ${index === 0 ? 'rounded-t' : ''
-                                    } ${index === userGroups.length - 1 ? 'rounded-b' : ''
-                                    }`}
-                            >
-                                <HashtagIcon className='h-6' />
-                                <span>{userGroup.group.name}</span>
-                            </button>
-                        ))}
-                    </div>
-                </div>
-                <div className='space-y-2 grow'>
-                    <div className='px-4 py-2 bg-neutral-800 rounded shadow-lg text-lg text-center'>
-                        Set Parent Server
-                    </div>
-                    <div className='bg-neutral-800 flex items-center space-x-4 w-full px-4 py-2 rounded shadow-lg'>
-                        <SolidStarIcon className='h-6' />
-                        <span>Arstotzka</span>
-                    </div>
-                    <div className='bg-neutral-800 rounded shadow-lg overflow-y-scroll h-96 scrollbar-none'>
-                        {userGroups.map((userGroup, index) => (
-                            <button
-                                key={userGroup.group.id}
-                                className={`flex items-center space-x-4 w-full px-4 py-2 hover:bg-neutral-600 ${index === 0 ? 'rounded-t' : ''
-                                    } ${index === userGroups.length - 1 ? 'rounded-b' : ''
-                                    }`}
-                            >
-                                <ServerIcon className='h-6' />
-                                <span>{userGroup.group.name}</span>
-                            </button>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            <div>{guild.name}</div>
         </div>
     )
 }
--- src/app/manage/guilds/page.tsx
+++ src/app/manage/guilds/page.tsx
@@ -2,47 +2,30 @@ import { APIGuild } from "discord-api-types/v10"
 import Image from 'next/image';
 import { auth } from "@/auth";
 import { redirect } from 'next/navigation';
+import { getBotGuild, getUserGuilds } from "@/lib/guilds";
 
 export const runtime = "edge";
 
-async function checkGuild(guild: APIGuild) {
-    const guildRes = await fetch(`https://discord.com/api/v10/guilds/${guild.id}/preview`, {
-        headers: {
-            Authorization: 'Bot ' + process.env.DISCORD_BOT_TOKEN,
-        }
-    });
+async function checkGuild(id: string) {
+    const targetGuild: APIGuild = await getBotGuild(id);
 
-    if (!guildRes.ok) {
-        redirect(`https://discord.com/api/oauth2/authorize?scope=bot+applications.commands&client_id=990855457885278208&permissions=8&guild_id=${guild.id}&disable_guild_select=true&redirect_uri=https://rolinker.net/api/auth/guild&response_type=code`)
+    if (!targetGuild) {
+        redirect(`https://discord.com/api/oauth2/authorize?scope=bot+applications.commands&client_id=990855457885278208&permissions=8&guild_id=${id}&disable_guild_select=true&redirect_uri=https://rolinker.net/api/auth/guild&response_type=code`);
+    } else {
+        redirect(`/manage/guilds/${id}`);
     };
-
-    redirect(`/manage/guilds/${guild.id}`)
 }
 
 export default async function Page() {
     const session = await auth();
 
-    const guildsRes = await fetch('https://discord.com/api/users/@me/guilds', {
-        headers: {
-            Authorization: 'Bearer ' + session?.user.access_token
-        }
-    });
-
-    if (!guildsRes.ok) {
-        throw new Error('Failed to fetch guilds');
-    };
-
-    const guilds: Array<APIGuild> = await guildsRes.json();
+    const guilds: Array<APIGuild> = await getUserGuilds(session?.user.access_token);
     const ownedGuilds: Array<APIGuild> = guilds.filter(guild => guild.owner === true);
 
     return (
         <div className='grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-2 w-full'>
             {ownedGuilds.length !== 0 && ownedGuilds.map((guild) => (
-                <form key={guild.id} className={`${ownedGuilds.length === 1 && 'col-span-full'}`} action={async () => {
-                    'use server';
-
-                    await checkGuild(guild);
-                }}>
+                <form key={guild.id} className={`${ownedGuilds.length === 1 && 'col-span-full'}`} action={async () => {'use server'; await checkGuild(guild.id);}}>
                     <button className={`flex items-center space-x-4 bg-neutral-800 w-full px-4 py-2 rounded shadow-lg hover:bg-neutral-700`}>
                         {guild.icon ? (
                             <Image src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png`} alt={`${guild.name} Icon`} className='h-16 w-16 rounded' width={100} height={100} />
