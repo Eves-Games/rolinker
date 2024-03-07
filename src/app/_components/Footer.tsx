5a58f5a0eee17d445e5d4d4e5fddc1c56df2fc6c
zekepari <pariezekiel@gmail.com>
2024-03-08 00:11:41 +1000

    reconstruction in new _components folder

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Link from "next/link";
+
+export default function Footer() {
+    return (
+        <footer className='flex flex-col gap-4 py-8 container'>
+            <div className='grid grid-cols-2 gap-8 md:grid-cols-4'>
+                <section className='flex flex-col gap-1'>
+                    <span className='font-bold text-xl'>RoLinker</span>
+                    <Link href='/'>Home</Link>
+                    <Link href='/commands'>Commands</Link>
+                </section>
+                <section className='flex flex-col gap-1'>
+                    <span className='font-bold text-xl'>Business</span>
+                    <Link href='/careers-and-commissions'>Careers and Commissions</Link>
+                </section>
+                <section className='flex flex-col gap-1'>
+                    <span className='font-bold text-xl'>Legal</span>
+                    <Link href='/privacy-policy'>Privacy Policy</Link>
+                    <Link href='/terms-of-service'>Terms of Service</Link>
+                </section>
+                <section className='flex flex-col gap-1'>
+                    <span className='font-bold text-xl'>Social</span>
+                    <Link href='https://discord.gg/CJDuGzwFX4' target='_blank'>Discord</Link>
+                </section>
+            </div>
+            <p className='text-sm text-neutral-400'>&copy; 2024 Eve&apos;s Games, SP. We are not affiliated with or endorsed by the Roblox Corporation.</p>
+        </footer>
+    )
+};
---
+++ src/app/_components/Nav.tsx
@@ -0,0 +1,51 @@
+import Link from 'next/link';
+import UserCard from './UserCard';
+import { auth, signIn } from '@/auth';
+import NavButton from '@/app/_components/NavButton';
+import { RoLinkerLogo } from '@/app/_components/RoLinkerLogo';
+import { DiscordLogo } from '@/app/_components/DiscordLogo';
+
+export interface NavLink {
+  name: string;
+  href: string;
+};
+
+const NavLinks = [
+  { name: 'Landing', href: '/' },
+  { name: 'Commands', href: '/commands' },
+  { name: 'Pricing', href: '/pricing' },
+  { name: 'Support', href: 'https://discord.gg/CJDuGzwFX4' },
+] satisfies Array<NavLink>;
+
+export default async function Nav() {
+  const session = await auth();
+
+  return (
+    <nav className='flex justify-between py-4 container'>
+      <div className='flex items-center gap-4'>
+        <div className='flex items-center gap-4 py-2'>
+          <RoLinkerLogo />
+          <span className='font-bold text-xl'>RoLinker</span>
+        </div>
+        <div className='hidden flex items-center gap-2 md:flex'>
+          {NavLinks.map((navLink, index) => (
+            <Link key={index} href={navLink.href} className='px-4 py-2 rounded hover:bg-neutral-800 hover:shadow-lg'>{navLink.name}</Link>
+          ))}
+        </div>
+      </div>
+      <div className='flex items-center gap-2'>
+        {session?.user ? (
+          <UserCard {...session.user} />
+        ) : (
+          <form action={async () => await signIn('discord')}>
+            <button className='flex items-center gap-4 px-4 py-2 rounded transition-colors bg-[#5865F2] hover:bg-opacity-75 whitespace-nowrap'>
+              <DiscordLogo />
+              <span>Sign in</span>
+            </button>
+          </form>
+        )}
+        <NavButton navLinks={NavLinks} />
+      </div >
+    </nav >
+  );
+};
