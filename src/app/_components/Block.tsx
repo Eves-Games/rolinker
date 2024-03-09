666fedd4d9a90ebea0b5e7a0f52de24f34397df7
zekepari <pariezekiel@gmail.com>
2024-03-09 21:20:40 +1000

    dont add padding on block component

--- src/app/_components/Block.tsx
+++ src/app/_components/Block.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export const Block: React.FC<{ href?: string, className?: string, children?: React.ReactNode }> = ({ href, className, children }) => {
     return href ? (
-        <Link href={href} className={className + ' bg-neutral-800 px-4 py-2 rounded shadow-lg hover:bg-neutral-700'}>
+        <Link href={href} className={className + ' bg-neutral-800 rounded shadow-lg hover:bg-neutral-700'}>
             {children}
         </Link>
     ) : (
-        <div className={className + ' bg-neutral-800 px-4 py-2 rounded shadow-lg'}>
+        <div className={className + ' bg-neutral-800 rounded shadow-lg'}>
             {children}
         </div>
     );
