84e8858b94fe86ebc78f5bb46b669070048eeb4e
zekepari <pariezekiel@gmail.com>
2024-03-12 23:46:42 +1000

    get account on account id

---
+++ src/app/api/account/[id]/route.ts
@@ -0,0 +1,21 @@
+import db from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    if (!params.id) return new NextResponse('No ID provided', {
+        status: 400,
+    });
+
+    const account = db.account.findUnique({
+        where: {
+            id: params.id,
+        }
+    });
+
+    return NextResponse.json(account);
+};
