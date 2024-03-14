4aa7a60f80210aaddcc067ccdcaf835ec49e49d5
zekepari <pariezekiel@gmail.com>
2024-03-15 02:03:08 +1000

    ' > "

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
-import { SessionProvider } from "next-auth/react";
-import Nav from "@/app/_components/Nav";
-import Footer from "@/app/_components/Footer"
-import { auth } from "@/auth";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
+import { SessionProvider } from 'next-auth/react';
+import Nav from '@/app/_components/Nav';
+import Footer from '@/app/_components/Footer'
+import { auth } from '@/auth';
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const open_sans = Open_Sans({
-  subsets: ["latin"],
+  subsets: ['latin'],
   display: 'swap',
 });
 
 export const metadata: Metadata = {
-  title: "RoLinker",
-  description: "Access your Roblox accounts on Discord",
+  title: 'RoLinker',
+  description: 'Access your Roblox accounts on Discord',
 };
 
 export default async function RootLayout({
@@ -26,7 +26,7 @@ export default async function RootLayout({
 
   return (
     <SessionProvider session={session}>
-      <html lang="en">
+      <html lang='en'>
         <body className={`${open_sans.className} bg-neutral-900 text-neutral-100 tracking-wide overflow-y-auto`}>
           <div className='min-h-screen'>
             <Nav />
@@ -35,8 +35,9 @@ export default async function RootLayout({
           </div>
           <hr className='border-neutral-800' />
           <Footer />
+          <SpeedInsights />
         </body>
       </html>
     </SessionProvider>
   );
-}
+};
