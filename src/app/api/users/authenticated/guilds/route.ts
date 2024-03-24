c9a3c43120f0f9522aea031a016284e7790ab366
zekepari <pariezekiel@gmail.com>
2024-03-24 23:01:40 +1000

    change api route to accomodate model removal

--- src/app/api/users/authenticated/guilds/route.ts
+++ src/app/api/users/authenticated/guilds/route.ts
@@ -11,17 +11,19 @@ export async function GET(request: NextRequest) {
         status: 401,
     });
 
-    const searchParams = request.nextUrl.searchParams
-    const query = searchParams.get('includeApiKeys')
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('includeApiKeys');
 
     const guilds = await db.guild.findMany({
-        where: {
-            ownerId: session.user.id
-        },
-        include: {
-            apiKey: query == 'true'
-        }
+        where: { ownerId: session.user.id }
     });
+
+    if (query !== 'true') {
+        guilds.forEach(guild => {
+            guild.apiKey = null;
+            guild.apiKeyUsage = 0;
+        });
+    };
 
     return NextResponse.json(guilds);
 };
