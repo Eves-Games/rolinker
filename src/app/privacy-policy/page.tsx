869336223bfca794bc4af074c13e8265c37c0ce7
zekepari <pariezekiel@gmail.com>
2024-03-27 19:29:45 +1000

    grammatical changes + definition of us, we, our

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -6,8 +6,8 @@ export default function Page() {
 		<section className='container space-y-4 mt-4 mb-16 max-w-screen-md'>
 			<h1 className='font-bold text-3xl mb-4'>Privacy Policy</h1>
 			<p>Last Updated: 27/03/2024</p>
-			<p>At Eve&apos;s Games, SP, the operator of RoLinker, we highly value the privacy of our users. This Privacy Policy outlines the collection, usage, and sharing of personal information gathered from users of the RoLinker service. By accessing or utilizing the RoLinker service, you explicitly acknowledge that you have reviewed, comprehended, and accepted the terms of this Privacy Policy. If you disagree with any part of this policy, you are not authorized to access or use the RoLinker service. Should you have any inquiries or concerns, please feel free to reach out to us.</p>
-			<hr/>
+			<p>At Eve&apos;s Games, SP (&quot;we&quot;, &quot;us&quot;, or &quot;our&quot;), the operator of RoLinker, we highly value the privacy of our users. This Privacy Policy outlines the collection, usage, and sharing of personal information gathered from users of the RoLinker service. By accessing or utilizing the RoLinker service, you explicitly acknowledge that you have reviewed, comprehended, and accepted the terms of this Privacy Policy. If you disagree with any part of this policy, you are not authorized to access or use the RoLinker service. Should you have any inquiries or concerns, please feel free to reach out to us.</p>
+			<hr />
 			<h2 className="text-2xl font-semibold">Information Collection</h2>
 			<p>RoLinker collects minimal user data necessary for providing our linking services.</p>
 			<p>We collect the following information from users:</p>
@@ -28,7 +28,7 @@ export default function Page() {
 			<h4 className='font-semibold'>Roblox Groups</h4>
 			<ul className='list-disc list-inside'>
 				<li>Navigate to the <Link className='text-blue-500 hover:underline' href='/manage/guilds'>manage guilds page</Link></li>
-				<li>Click on whichever Discord guild has intended the Roblox group linked too it</li>
+				<li>Click on whichever Discord guild has the intended the Roblox group linked too it</li>
 				<li>Open the group options disclosure</li>
 				<li>Select &apos;none&apos; from the Roblox group listbox</li>
 			</ul>
@@ -42,13 +42,13 @@ export default function Page() {
 				<li>Sign-out of this website</li>
 			</ul>
 			<p>By deleting all pieces of information, all personal information about you is deleted.</p>
-			<hr/>
+			<hr />
 			<h2 className="text-2xl font-semibold">Use of Information</h2>
 			<p>The collected information is solely used for the purpose of linking your Discord and Roblox accounts and groups/guilds. It helps us ensure secure and efficient account linking.</p>
-			<hr/>
+			<hr />
 			<h2 className="text-2xl font-semibold">Data Security</h2>
 			<p>We implement robust security measures to protect your data from unauthorized access, alteration, or destruction. Our commitment to data security is unwavering, and we continuously update our security practices to safeguard your information.</p>
-			<hr/>
+			<hr />
 			<h2 className="text-2xl font-semibold">Changes to This Policy</h2>
 			<p>We may update this Privacy Policy from time to time. Updates will be posted in the <Link className='text-blue-500 hover:underline' href='https://discord.gg/CJDuGzwFX4'>Discord support server</Link>, we encourage you to review it periodically. Your continued use of RoLinker after any changes signifies your acceptance of our updated policy.</p>
 		</section>
