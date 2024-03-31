0795f57b4b655da8bbc2eed6a746075d9ed0732b
zekepari <pariezekiel@gmail.com>
2024-03-31 13:32:35 +1000

    get divisions

--- src/lib/discord/commands/get-divisions.ts
+++ src/lib/discord/commands/get-divisions.ts
@@ -1,56 +1,70 @@
 import db from "@/lib/db";
-import { getGroups, getRoles, getUserRoleInGroup } from "@/lib/roblox";
-import { APIChatInputApplicationCommandInteraction, InteractionResponseType, RESTGetAPIGuildRolesResult, Routes } from "discord-api-types/v10";
+import { getGroups, getRoles, getUserRoleInGroup, getUserRoles } from "@/lib/roblox";
+import {
+    APIChatInputApplicationCommandInteraction,
+    APIInteractionResponse,
+    InteractionResponseType,
+    MessageFlags,
+    RESTGetAPIGuildRolesResult,
+    Routes,
+    APIMessageComponentEmoji,
+    ComponentType,
+    ButtonStyle,
+    APIInvite,
+} from "discord-api-types/v10";
 import { errorMessage, noLinkedAccounts, noLinkedGroup, notInGroup, successMessage } from "@/lib/discord/messages";
 import { rest } from "@/lib/discord/rest";
 import { findAssociatedAccount } from "@/lib/discord/util";
 
 export async function getDivisionsCommand(interaction: APIChatInputApplicationCommandInteraction) {
-    const { member, guild_id } = interaction
-
+    const { member, guild_id } = interaction;
     if (!guild_id || !member) return errorMessage(interaction, InteractionResponseType.ChannelMessageWithSource, 'Interaction objects not found');
 
-    const guild = await db.guild.findUnique({
-        where: {
-            id: guild_id
-        },
-        include: {
-            childGuilds: true,
-            parentGuild: true
-        }
-    });
-
+    const guild = await db.guild.findUnique({ where: { id: guild_id }, include: { childGuilds: true } });
     if (!guild?.groupId) return noLinkedGroup(InteractionResponseType.ChannelMessageWithSource);
 
     const account = await findAssociatedAccount(member.user.id, guild_id);
-
     if (!account) return noLinkedAccounts(InteractionResponseType.ChannelMessageWithSource);
 
-    const groupRanks = await getRoles(guild.groupId);
-    const userRank = await getUserRoleInGroup(account.id, guild.groupId);
-    const memberRoles = member.roles;
+    const userRanks = await getUserRoles(account.id);
+    if (!userRanks) {
+        return errorMessage(interaction, InteractionResponseType.ChannelMessageWithSource, 'User ranks not found');
+    }
 
-    if (!groupRanks || !userRank) {
-        const group = await getGroups([guild.groupId]);
+    const userGroupIds = userRanks.map(userRank => userRank.group.id);
+    const applicableGuilds = guild.childGuilds.filter(guild => guild.groupId && guild.inviteChannelId && userGroupIds.includes(parseInt(guild.groupId)));
 
-        if (!group) {
-            return notInGroup(InteractionResponseType.ChannelMessageWithSource, { name: 'this group', id: guild.groupId, })
-        } else {
-            return notInGroup(InteractionResponseType.ChannelMessageWithSource, group.data[0]);
-        }
-    };
+    const invites = await Promise.all(
+        applicableGuilds.map(async (guild) => {
+            const invite = await rest.post(Routes.channelInvites(guild.inviteChannelId!)).catch(() => null);
+            return { guild, invite };
+        })
+    );
 
-    const guildRolesData = await rest.get(Routes.guildRoles(guild_id)).catch(() => { return []; }) as RESTGetAPIGuildRolesResult;
+    const validInvites = invites.filter((invite): invite is { guild: typeof guild.childGuilds[number]; invite: APIInvite } => invite.invite !== null) as { guild: typeof guild.childGuilds[number]; invite: APIInvite }[];
 
-    const removeRanks = groupRanks.filter(rank => rank.id == userRank.id);
-    const removeRoles = guildRolesData.filter(role => removeRanks.some(rank => rank.name === role.name)).filter(role => memberRoles.includes(role.name));
-    const addRole = guildRolesData.find(role => role.name == userRank.name);
-
-    await rest.put(Routes.guildMemberRole(guild_id, member.user.id, addRole!.id)).catch();
-
-    for (const role of removeRoles) {
-        await rest.delete(Routes.guildMemberRole(guild_id, member.user.id, role.id)).catch();
-    };
-
-    return successMessage(InteractionResponseType.ChannelMessageWithSource);
+    return {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+            embeds: [
+                {
+                    title: 'Your Divisions',
+                    description: 'Click a button to join that division\'s Discord server.',
+                },
+            ],
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: validInvites.map(({ guild, invite }) => ({
+                        type: ComponentType.Button,
+                        style: ButtonStyle.Link,
+                        label: guild.name,
+                        url: `https://discord.gg/${invite}`,
+                        disabled: !invite,
+                    })),
+                },
+            ],
+            flags: MessageFlags.Ephemeral,
+        },
+    } satisfies APIInteractionResponse;
 }
