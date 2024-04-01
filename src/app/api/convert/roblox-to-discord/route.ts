f4a511eb66d48fe959b292426f5de27eaa2d460b
zekepari <pariezekiel@gmail.com>
2024-04-01 17:19:08 +1000

    roblox to discord api

---
+++ src/app/api/convert/roblox-to-discord/route.ts
@@ -0,0 +1,31 @@
+import { getDetailedAccounts } from "@/lib/accounts";
+import db from "@/lib/db";
+import { rest } from "@/lib/discord/rest";
+import { findAssociatedAccount } from "@/lib/discord/util";
+import { getGroups, getRoles, getUserRoleInGroup } from "@/lib/roblox";
+import { APIGuildMember, RESTGetAPIGuildRolesResult, Routes } from "discord-api-types/v10";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export async function GET(request: NextRequest) {
+    const searchParams = request.nextUrl.searchParams;
+    const query = searchParams.get('robloxId');
+
+    if (!query) return new NextResponse('No User ID key provided', {
+        status: 400,
+    });
+
+    const account = await db.account.findUnique({
+        where: {
+            id: query
+        }
+    });
+
+    if (!account) {
+        return new NextResponse('User not found', { status: 400, });
+    };
+
+    return NextResponse.json(account.userId);
+};
