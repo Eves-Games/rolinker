013f5004d6720617658877aed41c9ef338ad0cac
zekepari <pariezekiel@gmail.com>
2024-04-03 14:58:36 +1000

    cron job for resetting api keys

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,27 @@
+import db from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+        return new Response('Unauthorized', {
+            status: 401,
+        });
+    };
+
+    try {
+        await db.guild.updateMany({
+            data: {
+                apiKeyUsage: 0,
+            },
+        });
+
+        console.log('API key usage reset successfully');
+    } catch (error) {
+        console.error('Error resetting API key usage:', error);
+    };
+
+    return new NextResponse('Success', {
+        status: 200,
+    });
+};
