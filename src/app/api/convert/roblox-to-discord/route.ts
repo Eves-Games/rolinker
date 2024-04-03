a915f7ba7110a80aa1d2e21a8db3c63dde3b1bf1
zekepari <pariezekiel@gmail.com>
2024-04-03 19:51:10 +1000

    take api key on public convert

--- src/app/api/convert/roblox-to-discord/route.ts
+++ src/app/api/convert/roblox-to-discord/route.ts
@@ -10,6 +10,8 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
+    const headersList = headers();
+    const apiKeyHeader = headersList.get('Authorization');
     const searchParams = request.nextUrl.searchParams;
     const query = searchParams.get('robloxId');
 
@@ -17,10 +19,29 @@ export async function GET(request: NextRequest) {
         status: 400,
     });
 
+    if (!apiKeyHeader) return new NextResponse('No API key provided', {
+        status: 400,
+    });
+
+    const guildApiKey = await db.guild.findUnique({
+        where: { apiKey: apiKeyHeader }
+    });
+
+    if (!guildApiKey) return new NextResponse('Invalid API key', {
+        status: 401,
+    });
+
+    if (guildApiKey.apiKeyUsage == 750) return new NextResponse('API key usage limit reached (750)', {
+        status: 401,
+    });
+
+    db.guild.update({
+        where: { apiKey: apiKeyHeader },
+        data: { apiKeyUsage: { increment: 1 } }
+    }).catch();
+
     const account = await db.account.findUnique({
-        where: {
-            id: query
-        }
+        where: { id: query }
     });
 
     if (!account) {
