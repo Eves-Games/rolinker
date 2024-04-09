08b0f85ef3a8d1db8570358fc9ffd5e038df2c8b
zekepari <pariezekiel@gmail.com>
2024-04-09 14:56:01 +1000

    direct link to manage accounts on /link

--- src/lib/discord/commands/link.ts
+++ src/lib/discord/commands/link.ts
@@ -6,14 +6,8 @@ export async function linkCommand(interaction?: APIChatInputApplicationCommandIn
         data: {
             embeds: [
                 {
-                    title: 'Link a Roblox Account',
-                    url: 'https://rolinker.net',
-                    fields: [
-                        { name: 'Step 1', value: 'Tap on the title and accept the redirection to the RoLinker website.' },
-                        { name: 'Step 2', value: 'Tap the sign in button on the top right of the website, and sign in with Discord.' },
-                        { name: 'Step 3', value: "Tap your Discord username, and select 'Manage' from the dropdown." },
-                        { name: 'Step 4', value: 'Tap the plus (+) icon to add a new Roblox account.' },
-                    ],
+                    title: 'Click to link a Roblox Account',
+                    url: 'https://rolinker.net/manage/accounts'
                 },
             ],
             flags: MessageFlags.Ephemeral,
