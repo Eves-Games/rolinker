b984ea8cfbf96cab3c0a4ebe774c2a9af35b4710
zekepari <pariezekiel@gmail.com>
2024-05-05 23:16:09 +1000

    temporary return page which I need to reroute back to the api key page

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -1,27 +1,32 @@
+import db from "@/lib/db";
 import { stripe } from "@/lib/stripe";
+import Link from "next/link";
 
 async function getSession(sessionId: string) {
   const session = await stripe.checkout.sessions.retrieve(sessionId!);
   return session;
 }
 
-export default async function CheckoutReturn({ params }: { params: { session_id: string } }) {
-  const sessionId = params.session_id;
+export default async function CheckoutReturn({ searchParams }: { searchParams: { session_id: string } }) {
+  const sessionId = searchParams.session_id;
   const session = await getSession(sessionId);
 
-  console.log(session);
-
   if (session?.status === "open") {
-    return <p>Payment did not work.</p>;
+    return (
+      <div className='text-center my-4'>
+        <h1 className='font-bold text-3xl'>Payment failed!</h1>
+        <p>Try again or contact <Link className='text-blue-500 hover:underline' href='https://discord.gg/yV2sSRJ9h5'>support</Link>.</p>
+      </div>
+    )
   }
 
   if (session?.status === "complete") {
     return (
-      <h3>
-        We appreciate your business! Your Stripe customer ID is:
-        {(session.customer as string)}.
-      </h3>
-    );
+      <div className='text-center my-4'>
+        <h1 className='font-bold text-3xl'>Payment success!</h1>
+        <p>Thank-you for your business.</p>
+      </div>
+    )
   }
 
   return null;
