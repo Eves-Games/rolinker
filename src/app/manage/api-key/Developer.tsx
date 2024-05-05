c8e63dd46b6f19a3327111848ef16081d9b55ffb
zekepari <pariezekiel@gmail.com>
2024-05-05 23:15:44 +1000

    look better

--- src/app/manage/api-key/Developer.tsx
+++ src/app/manage/api-key/Developer.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from "react";
-import { ArrowTopRightOnSquareIcon, ChartBarIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon, CodeBracketIcon, PencilIcon } from "@heroicons/react/24/outline";
+import { ArrowTopRightOnSquareIcon, ChartBarIcon, ChevronDoubleUpIcon, CodeBracketIcon, PencilIcon, Bars2Icon } from "@heroicons/react/24/outline";
 import EditKeyDialog from "./_components/EditKeyDialog"
 import ViewUsageDialog from "./_components/ViewUsageDialog";
 import PremiumDialog from "./_components/PremiumDialog";
@@ -51,6 +51,18 @@ export default function Page({
                 </div>
                 <div className='gap-4 grid grid-cols-1 md:grid-cols-2'>
                     <div className='space-y-2'>
+                        <h2 className='text-xl font-semibold'>Premium Plan</h2>
+                        <ul className='list-disc list-inside'>
+                            <li className='font-semibold text-green-500'>Unlimited Requests</li>
+                            <li>Access all Endpoints</li>
+                            <li>1-on-1 Support</li>
+                        </ul>
+                        <button onClick={() => setPremiumDialogOpen(true)} className={`flex justify-between space-x-4 bg-indigo-700 rounded-lg py-2 px-4 shadow-lg ${false ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-600'}`}>
+                            <span className='truncate'>Select Premium</span>
+                            <ChevronDoubleUpIcon className='size-6 flex-shrink-0' aria-hidden='true' />
+                        </button>
+                    </div>
+                    <div className='space-y-2'>
                         <h2 className='text-xl font-semibold'>Free Plan (Current)</h2>
                         <ul className='list-disc list-inside'>
                             <li>750 Requests Daily</li>
@@ -59,19 +71,7 @@ export default function Page({
                         </ul>
                         <button className={`flex justify-between space-x-4 bg-neutral-700 rounded-lg py-2 px-4 shadow-lg ${true ? 'opacity-50 cursor-not-allowed' : 'hover:bg-neutral-600'}`}>
                             <span className='truncate'>Select Free</span>
-                            <ChevronDoubleDownIcon className='size-6 flex-shrink-0' aria-hidden='true' />
-                        </button>
-                    </div>
-                    <div className='space-y-2'>
-                        <h2 className='text-xl font-semibold'>Premium Plan</h2>
-                        <ul className='list-disc list-inside'>
-                            <li className='font-semibold text-green-500'>Unlimited Requests</li>
-                            <li>Access all Endpoints</li>
-                            <li>1-on-1 Support</li>
-                        </ul>
-                        <button onClick={() => setPremiumDialogOpen(true)} className={`flex justify-between space-x-4 bg-indigo-700 rounded-lg py-2 px-4 shadow-lg ${false ? 'opacity-50 cursor-not-allowed' : 'hover:bg-indigo-600'}`}>
-                            <span className='truncate'>Get Premium</span>
-                            <ChevronDoubleUpIcon className='size-6 flex-shrink-0' aria-hidden='true' />
+                            <Bars2Icon className='size-6 flex-shrink-0' aria-hidden='true' />
                         </button>
                     </div>
                 </div>
