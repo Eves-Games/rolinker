3fa018750f4fda827ea6a8e2fedc364d6bc2efea
zekepari <pariezekiel@gmail.com>
2024-05-07 00:36:31 +1000

    refactors

--- src/app/settings/account/page.tsx
+++ src/app/settings/account/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { ArrowTopRightOnSquareIcon, TrashIcon, UserCircleIcon } from '@heroicons/react/24/outline';
+import { ArrowTopRightOnSquareIcon, DocumentChartBarIcon, TrashIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { deleteAccount } from './actions';
 import { signOut } from 'next-auth/react';
@@ -12,18 +12,18 @@ export default function Page() {
     const isInputValid = inputValue === 'Delete my account';
 
     return (
-        <>
+        <div className='w-full space-y-2'>
             <div className='bg-neutral-800 rounded shadow-lg w-full px-4 py-2 space-y-4'>
                 <div className='flex items-center space-x-4'>
-                    <UserCircleIcon className='size-6' />
-                    <span>Your Account</span>
+                    <DocumentChartBarIcon className='size-6' />
+                    <span>Your Data</span>
                 </div>
                 <div className='gap-4 grid grid-cols-1 md:grid-cols-1'>
                     <div className='space-y-2 relative'>
                         <span>Delete Account</span>
                         <form action={deleteAccount} className='flex space-x-2'>
                             <input onChange={(e) => setInputValue(e.target.value)} value={inputValue} type='text' placeholder="Enter 'Delete my account'" className='flex justify-between space-x-4 w-full bg-neutral-700 hover:bg-neutral-600 rounded-lg py-2 px-4 shadow-lg' />
-                            <button onClick={() => signOut({redirect: true, callbackUrl: '/'})} className={`flex justify-between space-x-4 bg-red-600 rounded-lg py-2 px-4 shadow-lg ${!isInputValid ? 'opacity-50 cursor-not-allowed' : 'hover:bg-red-500'}`} disabled={!isInputValid}>
+                            <button onClick={() => signOut({ redirect: true, callbackUrl: '/' })} className={`flex justify-between space-x-4 bg-red-600 rounded-lg py-2 px-4 shadow-lg ${!isInputValid ? 'opacity-50 cursor-not-allowed' : 'hover:bg-red-500'}`} disabled={!isInputValid}>
                                 <span className='truncate'>Delete Account</span>
                                 <TrashIcon className='size-6 flex-shrink-0' aria-hidden='true' />
                             </button>
@@ -32,6 +32,6 @@ export default function Page() {
                 </div>
                 <p className='text-center'>This will delete all your data. <Link href='/privacy-policy' className='text-blue-500 hover:underline' target='_blank'>Privacy Policy <ArrowTopRightOnSquareIcon className="size-5 inline-block" /></Link></p>
             </div>
-        </>
+        </div>
     );
 }
--- src/app/settings/layout.tsx
+++ src/app/settings/layout.tsx
@@ -1,27 +1,12 @@
 import Link from "next/link";
-import { ExclamationTriangleIcon, UserIcon, WalletIcon } from '@heroicons/react/24/outline'
+import { UserIcon, WalletIcon } from '@heroicons/react/24/outline'
 import Block from "../_components/Block";
-import { auth } from "@/auth";
-import Image from 'next/image';
 
-export default async function Layout({
+export default function Layout({
     children,
 }: {
     children: React.ReactNode
 }) {
-    const session = await auth();
-
-    if (!session) {
-        return (
-            <section className='container my-4 max-w-screen-lg'>
-                <div className='flex justify-center items-center space-x-4 border-dashed border-4 border-neutral-800 rounded shadow-lg w-full h-20'>
-                    <ExclamationTriangleIcon className='size-6' />
-                    <span>Unauthorized</span>
-                </div>
-            </section>
-        );
-    }
-
     return (
         <section className='container my-4 max-w-screen-lg'>
             <h1 className='font-bold text-3xl mb-4'>Settings</h1>
@@ -37,13 +22,7 @@ export default async function Layout({
                         <span>Billing</span>
                     </Link>
                 </Block>
-                <div className='w-full space-y-2'>
-                    <div className='flex items-center space-x-4 bg-neutral-800 px-4 py-2 rounded shadow-lg w-full' key={session.user.image}>
-                        <Image src={session.user.image} alt={`${session.user.name} Icon`} className='size-16 rounded-full' width={100} height={100} />
-                        <span className='text-lg'>{session.user.name}</span>
-                    </div>
-                    {children}
-                </div>
+                {children}
             </div>
         </section>
     )
