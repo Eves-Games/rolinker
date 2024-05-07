002749759fa829b10c74ed096a07b381bd04007f
zekepari <pariezekiel@gmail.com>
2024-05-07 22:39:26 +1000

    remove session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
-import { SessionProvider } from 'next-auth/react';
 import Nav from '@/app/_components/Nav';
 import Footer from '@/app/_components/Footer'
-import { auth } from '@/auth';
 
 const open_sans = Open_Sans({
   subsets: ['latin'],
@@ -21,21 +19,17 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await auth();
-
   return (
-    <SessionProvider session={session}>
-      <html lang='en'>
-        <head></head>
-        <body className={`${open_sans.className} bg-neutral-900 text-neutral-100 tracking-wide overflow-y-auto`}>
-          <div className='min-h-screen'>
-            <Nav />
-            {children}
-          </div>
-          <hr className='border-neutral-800' />
-          <Footer />
-        </body>
-      </html>
-    </SessionProvider>
+    <html lang='en'>
+      <head></head>
+      <body className={`${open_sans.className} bg-neutral-900 text-neutral-100 tracking-wide overflow-y-auto`}>
+        <div className='min-h-screen'>
+          <Nav />
+          {children}
+        </div>
+        <hr className='border-neutral-800' />
+        <Footer />
+      </body>
+    </html>
   );
 };
