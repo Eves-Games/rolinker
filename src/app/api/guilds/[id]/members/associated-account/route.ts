ad5e4919a04a80356505d09b1bc46654cad0e9ec
zekepari <pariezekiel@gmail.com>
2024-05-12 20:06:27 +1000

    refactor

--- src/app/api/guilds/[id]/members/associated-account/route.ts
+++ src/app/api/guilds/[id]/members/associated-account/route.ts
@@ -12,48 +12,29 @@ export async function GET(
     { params }: { params: { id: string } }
 ) {
     const headersList = headers();
-    const apiKeyHeader = headersList.get('Authorization');
     const searchParams = request.nextUrl.searchParams;
+
+    const apiKeyHeader = headersList.get('Authorization');
     const memberId = searchParams.get('memberId')
     const guildId = params.id
 
-    if (!memberId || !guildId) return new NextResponse('No User ID key provided', {
-        status: 400,
-    });
+    if (!memberId) return new NextResponse('No member ID provided', { status: 400 });
+    if (!apiKeyHeader) return new NextResponse('No API key provided', { status: 400 });
 
-    if (!apiKeyHeader) return new NextResponse('No API key provided', {
-        status: 400,
-    });
-
-    const apiKey = await db.apiKey.findUnique({
-        where: { key: apiKeyHeader }
-    });
-
-    if (!apiKey) return new NextResponse('Invalid API key', {
-        status: 401,
-    });
-
-    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', {
-        status: 401,
-    });
+    const apiKey = await db.apiKey.findUnique({ where: { key: apiKeyHeader } });
+    if (!apiKey) return new NextResponse('Invalid API key', { status: 401 });
+    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', { status: 429 });
 
     db.apiKey.update({
         where: { key: apiKeyHeader },
         data: { usage: { increment: 1 } }
     }).catch();
 
-    const member = await rest.get(Routes.guildMember(guildId, memberId)).catch(() => { return null; }) as APIGuildMember;
-
-    if (!member) {
-        return new NextResponse('User not apart of guild API key is linked too', { status: 404, });
-    };
+    const member = await rest.get(Routes.guildMember(guildId, memberId)).catch(() => { return null; }) as APIGuildMember | null;
+    if (!member) return new NextResponse('User not apart of guild API key is linked too', { status: 404 });
 
     const account = await findAssociatedAccount(memberId, guildId);
+    if (!account) return new NextResponse('User has no linked Roblox accounts', { status: 404 });
 
-    if (account) {
-        const { userId, ...refinedAccount } = account;
-        return NextResponse.json(refinedAccount);
-      } else {
-        return new NextResponse('Associated account not found', { status: 404 });
-      }
+    return NextResponse.json(account);
 };
--- src/app/api/guilds/[id]/members/update-roles/route.ts
+++ src/app/api/guilds/[id]/members/update-roles/route.ts
@@ -2,7 +2,7 @@ import db from "@/lib/db";
 import { rest } from "@/lib/discord/rest";
 import { findAssociatedAccount } from "@/lib/discord/util";
 import { getGroupRoles, getUserRoles } from "@/lib/roblox";
-import { APIGuildMember, RESTGetAPIGuildResult, RESTGetAPIGuildRolesResult, Routes } from "discord-api-types/v10";
+import { APIGuildMember, RESTGetAPIGuildResult, Routes } from "discord-api-types/v10";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -13,82 +13,51 @@ export async function POST(
     { params }: { params: { id: string } }
 ) {
     const headersList = headers();
-    const apiKeyHeader = headersList.get('Authorization');
     const searchParams = request.nextUrl.searchParams;
+
+    const apiKeyHeader = headersList.get('Authorization');
     const memberId = searchParams.get('memberId')
     const guildId = params.id
 
-    if (!memberId || !guildId) return new NextResponse('No User ID key provided', {
-        status: 400,
-    });
+    if (!memberId) return new NextResponse('No member ID provided', { status: 400 });
+    if (!apiKeyHeader) return new NextResponse('No API key provided', { status: 400 });
 
-    if (!apiKeyHeader) return new NextResponse('No API key provided', {
-        status: 400,
-    });
-
-    const apiKey = await db.apiKey.findUnique({
-        where: { key: apiKeyHeader }
-    });
-
-    if (!apiKey) return new NextResponse('Invalid API key', {
-        status: 401,
-    });
-
-    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', {
-        status: 401,
-    });
+    const apiKey = await db.apiKey.findUnique({ where: { key: apiKeyHeader } });
+    if (!apiKey) return new NextResponse('Invalid API key', { status: 401 });
+    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', { status: 429 });
 
     db.apiKey.update({
         where: { key: apiKeyHeader },
         data: { usage: { increment: 1 } }
     }).catch();
 
-    const apiGuild = await rest.get(Routes.guild(guildId)).catch(() => { return []; }) as RESTGetAPIGuildResult;
-
-    if (apiGuild.owner_id !== apiKey.userId) return new NextResponse('Unauthorized API key', {
-        status: 401,
-    });
+    const botGuild = await rest.get(Routes.guild(guildId)).catch(() => { return null; }) as RESTGetAPIGuildResult | null;
+    if (!botGuild) return new NextResponse('Could not find guild', { status: 404 });
+    if (botGuild.owner_id !== apiKey.userId) return new NextResponse('Unauthorized API key', { status: 401 });
 
     const guild = await db.guild.findUnique({ where: { id: guildId } });
+    if (!guild || !guild.groupId) return new NextResponse('Guild is not linked to a Roblox group', { status: 404 });
 
-    if (!guild || !guild.groupId) return new NextResponse('Guild is not linked to a Roblox group', {
-        status: 400,
-    });
-
-    const member = await rest.get(Routes.guildMember(guildId, memberId)).catch(() => { return null; }) as APIGuildMember;
-
-    if (!member) {
-        return new NextResponse('User not apart of guild API key is linked too', { status: 404, });
-    }
+    const member = await rest.get(Routes.guildMember(guildId, memberId)).catch(() => { return null; }) as APIGuildMember | null;
+    if (!member) return new NextResponse('User not apart of guild API key is linked too', { status: 404 });
 
     const account = await findAssociatedAccount(memberId, guildId);
-
-    if (!account) return new NextResponse('User has no linked Roblox accounts', {
-        status: 400,
-    });
+    if (!account) return new NextResponse('User has no linked Roblox accounts', { status: 400 });
 
     const groupRoles = await getGroupRoles(guild.groupId);
     const userRoles = await getUserRoles(account.id);
     const userRole = userRoles.data.find(role => role.group.id === parseInt(guild.groupId!));
     const memberRoles = member.roles;
 
-    if (!groupRoles || !userRole) {
-        return new NextResponse('User is not in linked Roblox group', {
-            status: 400,
-        });
-    };
+    if (!groupRoles || !userRole) return new NextResponse('User is not in linked Roblox group', { status: 400 });
 
     const removeRanks = groupRoles.roles.filter(role => role.id == userRole.role.id);
-    const removeRoles = apiGuild.roles.filter(role => removeRanks.some(rank => rank.name === role.name)).filter(role => memberRoles.includes(role.name));
-    const addRole = apiGuild.roles.find(role => role.name == userRole.role.name);
+    const removeRoles = botGuild.roles.filter(role => removeRanks.some(rank => rank.name === role.name)).filter(role => memberRoles.includes(role.name));
+    const addRole = botGuild.roles.find(role => role.name == userRole.role.name);
 
     await rest.put(Routes.guildMemberRole(guildId, member.user!.id, addRole!.id)).catch();
 
-    for (const role of removeRoles) {
-        await rest.delete(Routes.guildMemberRole(guildId, member.user!.id, role.id)).catch();
-    };
+    for (const role of removeRoles) await rest.delete(Routes.guildMemberRole(guildId, member.user!.id, role.id)).catch();
 
-    return new NextResponse('Success', {
-        status: 200,
-    });
+    return new NextResponse('Success');
 };
