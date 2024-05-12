638932e621aa4bda14e09535c52baa5a90e84db2
zekepari <pariezekiel@gmail.com>
2024-05-12 20:06:37 +1000

    new shout route

---
+++ src/app/api/guilds/[id]/roblox-bot/shout/route.ts
@@ -0,0 +1,36 @@
+import db from "@/lib/db";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import noblox from "noblox.js";
+
+export async function POST(
+    request: NextRequest,
+    { params }: { params: { id: string } }
+) {
+    const headersList = headers();
+
+    const apiKeyHeader = headersList.get('Authorization');
+    const guildId = params.id
+
+    if (!apiKeyHeader) return new NextResponse('No API key provided', { status: 400 });
+
+    const apiKey = await db.apiKey.findUnique({ where: { key: apiKeyHeader } });
+    if (!apiKey) return new NextResponse('Invalid API key', { status: 401 });
+    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', { status: 429 });
+
+    db.apiKey.update({
+        where: { key: apiKeyHeader },
+        data: { usage: { increment: 1 } }
+    }).catch();
+
+    const guild = await db.guild.findUnique({ where: { id: guildId } });
+    if (!guild || !guild.groupId) return new NextResponse('Guild is not linked to a Roblox group', { status: 404 });
+    if (!guild.robloxCookie) return new NextResponse('Guild is not linked to a Roblox group', { status: 404 });
+
+    try {
+        await noblox.shout(parseInt(guild.groupId), 'shout', { session: guild.robloxCookie, });
+        return new NextResponse('Success');
+    } catch (err) {
+        return NextResponse.json(err, { status: 500 })
+    };
+};
