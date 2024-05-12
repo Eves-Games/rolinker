fc0093337ee4e13eec98c1d2e3ef2f52464f642d
zekepari <pariezekiel@gmail.com>
2024-05-12 21:00:10 +1000

    use bloxy instead of noblox

--- src/app/api/guilds/[id]/roblox-bot/shout/route.ts
+++ src/app/api/guilds/[id]/roblox-bot/shout/route.ts
@@ -1,7 +1,7 @@
 import db from "@/lib/db";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import noblox from "noblox.js";
+import bloxy from "bloxy";
 
 export async function POST(
     request: NextRequest,
@@ -28,7 +28,9 @@ export async function POST(
     if (!guild.robloxCookie) return new NextResponse('Guild is not linked to a Roblox group', { status: 404 });
 
     try {
-        await noblox.shout(parseInt(guild.groupId), 'shout', { session: guild.robloxCookie, });
+        const client = await new bloxy.Client({ credentials: { cookie: guild.robloxCookie } });
+        const group = await client.getGroup(parseInt(guild.groupId));
+        await group.updateShout('shout');
         return new NextResponse('Success');
     } catch (err) {
         return NextResponse.json(err, { status: 500 })
