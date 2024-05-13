af603a40c4da2ca4e289f400e2c7119715d0af72
zekepari <pariezekiel@gmail.com>
2024-05-13 21:09:00 +1000

    remove roblox cookie from get guild!!

--- src/app/api/guilds/[id]/route.ts
+++ src/app/api/guilds/[id]/route.ts
@@ -16,25 +16,13 @@ export async function GET(
     const memberId = searchParams.get('memberId')
     const guildId = params.id
 
-    if (!memberId || !guildId) return new NextResponse('No User ID key provided', {
-        status: 400,
-    });
+    if (!memberId || !guildId) return new NextResponse('No User ID key provided', { status: 400, });
+    if (!apiKeyHeader) return new NextResponse('No API key provided', { status: 400, });
 
-    if (!apiKeyHeader) return new NextResponse('No API key provided', {
-        status: 400,
-    });
+    const apiKey = await db.apiKey.findUnique({ where: { key: apiKeyHeader } });
 
-    const apiKey = await db.apiKey.findUnique({
-        where: { key: apiKeyHeader }
-    });
-
-    if (!apiKey) return new NextResponse('Invalid API key', {
-        status: 401,
-    });
-
-    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', {
-        status: 401,
-    });
+    if (!apiKey) return new NextResponse('Invalid API key', { status: 401, });
+    if (apiKey.usage == 750) return new NextResponse('API key usage limit reached (750)', { status: 401, });
 
     db.apiKey.update({
         where: { key: apiKeyHeader },
@@ -43,13 +31,14 @@ export async function GET(
 
     const apiGuild = await rest.get(Routes.guild(guildId)).catch(() => { return []; }) as RESTGetAPIGuildResult;
 
-    if (apiGuild.owner_id !== apiKey.userId) return new NextResponse('Unauthorized API key', {
-        status: 401,
-    });
+    if (apiGuild.owner_id !== apiKey.userId) return new NextResponse('Unauthorized API key', { status: 401, });
 
-    const guild = await db.guild.findUnique({
-        where: { id: guildId }
-    });
+    const guild = await db.guild.findUnique({ where: { id: guildId } });
+
+    if (guild) {
+        const { robloxCookie, ...guildWithoutCookie } = guild;
+        return NextResponse.json(guildWithoutCookie);
+      }
     
-    return NextResponse.json(guild);
+      return NextResponse.json(null);
 };
