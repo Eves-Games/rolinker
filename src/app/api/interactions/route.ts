c8fbb41653b927e5c42cfc9a6ae098b543383c75
zekepari <pariezekiel@gmail.com>
2024-05-14 01:21:59 +1000

    handle interactions properly for sub-commands

--- src/app/api/interactions/route.ts
+++ src/app/api/interactions/route.ts
@@ -25,9 +25,7 @@ export async function POST(request: Request) {
     };
 
     if (interaction.type === InteractionType.ApplicationCommand) {
-        const { name } = interaction.data
-        console.log(interaction)
-        console.log(interaction.data)
+        const { name, options } = interaction.data
         switch (name) {
             case commands.link.name:
                 return NextResponse.json(await linkCommand(interaction));
@@ -35,16 +33,32 @@ export async function POST(request: Request) {
                 return NextResponse.json(await switchCommand(interaction));
             case commands.sendlink.name:
                 return NextResponse.json(await sendLinkCommand(interaction));
-            case commands.bot.options[0].name: // shout
-                return NextResponse.json(await shoutCommand(interaction));
-            case commands.bot.options[1].name: // promote
-                return NextResponse.json(await promoteCommand(interaction));
-            case commands.bot.options[2].name: // demote
-                return NextResponse.json(await shoutCommand(interaction));
-            case commands.get.options[0].name: // get divisions
-                return NextResponse.json(await getDivisionsCommand(interaction));
-            case commands.get.options[1].name: // get roles
-                return NextResponse.json(await getRolesCommand(interaction));
+            case commands.bot.name:
+                if (!options) return new NextResponse("Options expected", { status: 400 });
+
+                for (const option of options) {
+                    const { name } = option
+                    switch (name) {
+                        case commands.bot.options[0].name: // shout
+                            return NextResponse.json(await shoutCommand(interaction));
+                        case commands.bot.options[1].name: // promote
+                            return NextResponse.json(await promoteCommand(interaction));
+                        case commands.bot.options[2].name: // demote
+                            return NextResponse.json(await shoutCommand(interaction));
+                    }
+                };
+            case commands.get.name:
+                if (!options) return new NextResponse("Options expected", { status: 400 });
+
+                for (const option of options) {
+                    const { name } = option
+                    switch (name) {
+                        case commands.get.options[0].name: // get divisions
+                            return NextResponse.json(await getDivisionsCommand(interaction));
+                        case commands.get.options[1].name: // get roles
+                            return NextResponse.json(await getRolesCommand(interaction));
+                    }
+                };
             default:
                 return new NextResponse("Unknown command", { status: 400 });
         }
