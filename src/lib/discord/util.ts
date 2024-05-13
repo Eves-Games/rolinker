88da5b9024832120a8ffd81f93740fcf23a1b28b
zekepari <pariezekiel@gmail.com>
2024-05-13 19:30:06 +1000

    refactor util

--- src/lib/discord/util.ts
+++ src/lib/discord/util.ts
@@ -4,27 +4,15 @@ import { Account, Guild } from "@prisma/client/edge";
 export async function findAssociatedAccount(userId: string, guildId: string) {
     let account: Account | null;
     const accountGuild = await db.accountGuild.findUnique({
-        where: {
-            userId_guildId: {
-                userId,
-                guildId
-            }
-        },
-        include: {
-            account: true
-        }
+        where: { userId_guildId: { userId, guildId } },
+        include: { account: true }
     });
 
     if (accountGuild) {
         account = accountGuild.account
     } else {
         account = await db.account.findUnique({
-            where: {
-                onePrimaryAccountPerUser: {
-                    userId,
-                    isPrimary: true
-                }
-            }
+            where: { onePrimaryAccountPerUser: { userId, isPrimary: true } }
         });
     };
 
@@ -36,9 +24,7 @@ export async function getRelatedGuilds(guildId: string): Promise<Guild[]> {
     const relatedGuilds: Guild[] = [];
 
     async function fetchRelatedGuilds(currentGuildId: string) {
-        if (visitedGuildIds.has(currentGuildId)) {
-            return;
-        }
+        if (visitedGuildIds.has(currentGuildId)) return;
 
         visitedGuildIds.add(currentGuildId);
 
@@ -52,14 +38,8 @@ export async function getRelatedGuilds(guildId: string): Promise<Guild[]> {
 
         if (guild) {
             relatedGuilds.push(guild);
-
-            if (guild.parentGuild) {
-                await fetchRelatedGuilds(guild.parentGuild.id);
-            }
-
-            for (const childGuild of guild.childGuilds) {
-                await fetchRelatedGuilds(childGuild.id);
-            }
+            if (guild.parentGuild) await fetchRelatedGuilds(guild.parentGuild.id);
+            for (const childGuild of guild.childGuilds) await fetchRelatedGuilds(childGuild.id);
         }
     }
 
