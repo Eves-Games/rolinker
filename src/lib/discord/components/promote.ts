28a95d615ec00fce2c88f7977dc778a4cc1fd70d
zekepari <pariezekiel@gmail.com>
2024-05-14 12:33:55 +1000

    promote test if roles are given in order

--- src/lib/discord/components/promote.ts
+++ src/lib/discord/components/promote.ts
@@ -1,11 +1,15 @@
 import db from '@/lib/db';
 import { InteractionResponseType, MessageFlags, APIModalSubmitInteraction, APIInteractionResponse } from 'discord-api-types/v10';
-import { generateMessage, MessageTitles, MessageColors } from '@/lib/discord/messages';
+import { generateMessage, MessageTitles, MessageColors, noLinkedAccounts } from '@/lib/discord/messages';
 import { Client } from 'bloxy';
+import { findAssociatedAccount } from '../util';
 
 export async function promoteComponent(interaction: APIModalSubmitInteraction) {
     const { guild_id, member } = interaction;
     if (!member || !guild_id) return generateMessage({responseType: InteractionResponseType.UpdateMessage, title: MessageTitles.Error, error: { interaction, message: 'Interaction objects not found' }});
+
+    const account = await findAssociatedAccount(member.user.id, guild_id);
+    if (!account) return noLinkedAccounts(InteractionResponseType.ChannelMessageWithSource);
 
     const guild = await db.guild.findUnique({ where: { id: guild_id } });
     if (!guild || !guild.groupId) return generateMessage({ responseType: InteractionResponseType.ChannelMessageWithSource, title: MessageTitles.NoGroupId, flags: MessageFlags.Ephemeral });
@@ -26,7 +30,18 @@ export async function promoteComponent(interaction: APIModalSubmitInteraction) {
 
     try {
         const group = await client.getGroup(parseInt(guild.groupId));
-        await group.updateShout(promoteReason);
+        const member = await group.getMember(parseInt(account.id));
+        const targetUserId = await client.getUserIdFromUsername(promoteTarget);
+        const targetMember = await group.getMember(targetUserId.id);
+        if (!member || !targetMember) throw new Error('No member');
+
+        const role = member.role
+        const targetRole = targetMember.role
+        if (!role || !role.rank || !targetRole || !targetRole.rank) throw new Error('No role');
+        if (role.rank <= targetRole.rank) return generateMessage({ responseType: InteractionResponseType.ChannelMessageWithSource, title: MessageTitles.NoPermission, flags: MessageFlags.Ephemeral });
+
+        const roles = await group.getRoles();
+        console.log(roles);
     } catch (err: any) {
         console.log(err)
         return generateMessage({ responseType: InteractionResponseType.ChannelMessageWithSource, title: MessageTitles.UnableShout, color: MessageColors.Red });
