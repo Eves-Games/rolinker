5e251267b8b47a3afc40cf11bd3866f437371f77
zekepari <pariezekiel@gmail.com>
2024-05-19 18:59:31 +1000

    fix redirect to privacy policy

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,15 @@ const nextConfig = {
             { hostname: 'cdn.discordapp.com' },
             { hostname: 'tr.rbxcdn.com' }
         ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/privacy-policy',
+                destination: 'https://www.iubenda.com/privacy-policy/42805761',
+                permanent: true
+            }
+        ]
     }
 };
 
