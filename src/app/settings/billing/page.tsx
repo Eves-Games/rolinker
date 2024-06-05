f4ddf7eda5e061687b5127d4e44812d296c23b59
Zeke Pari <68903556+zekepari@users.noreply.github.com>
2024-06-06 03:09:27 +1000

    no subscriptions found

--- src/app/settings/billing/page.tsx
+++ src/app/settings/billing/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import Card from "@/components/Card";
-import { Code2, Coins } from "lucide-react";
+import db from "@/db";
+import { CircleSlash, Code2, Coins } from "lucide-react";
 import { Metadata } from "next";
 
 export const revalidate = false;
@@ -15,6 +16,10 @@ export default async function Page() {
   if (!session || !session.user)
     throw new Error("Could not get session! Try signing in!");
 
+  const keyData = await db.apiKey.findUnique({
+    where: { userId: session.user.id },
+  });
+
   return (
     <section className="w-full space-y-2">
       <Card>
@@ -23,18 +28,27 @@ export default async function Page() {
           Active Subscriptions
         </h2>
       </Card>
-      <Card>
-        <h2 className="card-title">
-          <Code2 className="size-6" />
-          API Premium Plan
-          <span className="badge badge-neutral badge-lg">$2.99/m</span>
-        </h2>
-        <div className="card-actions justify-end">
-          <label htmlFor="free_plan_modal" className="btn btn-neutral">
-            Cancel Subscription
-          </label>
-        </div>
-      </Card>
+      {keyData?.premium ? (
+        <Card>
+          <h2 className="card-title">
+            <Code2 className="size-6" />
+            API Premium Plan
+            <span className="badge badge-neutral badge-lg">$2.99/m</span>
+          </h2>
+          <div className="card-actions justify-end">
+            <label htmlFor="free_plan_modal" className="btn btn-neutral">
+              Cancel Subscription
+            </label>
+          </div>
+        </Card>
+      ) : (
+        <Card>
+          <h2 className="card-title">
+            <CircleSlash className="size-6" />
+            No active subscriptions found
+          </h2>
+        </Card>
+      )}
     </section>
   );
 }
